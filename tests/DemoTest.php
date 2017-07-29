d6bbc995c004fa82ac585ddd23db9c77005a86d1
wangyi <chunhei2008@qq.com>
2017-07-29 10:09:06 +0800

    add tests project

---
+++ tests/DemoTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Demo\Test;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * DemoTest.php
+ *
+ * Author: wangyi <chunhei2008@qq.com>
+ *
+ * Date:   2017/7/28 22:22
+ * Copyright: (C) 2014, Guangzhou YIDEJIA Network Technology Co., Ltd.
+ */
+class DemoTest extends TestCase
+{
+
+    public function testDemo(){
+
+        $this->assertTrue(true);
+
+    }
+}
